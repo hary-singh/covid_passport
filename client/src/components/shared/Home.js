351a6bcba7c5b579df4df13ed4396e741256fd65
Spencer Curtis <spencercurtis@Spencers-MacBook-Air.local>
2021-04-10 10:28:35 -0600

    auth stuff

---
+++ client/src/components/shared/Home.js
@@ -0,0 +1,7 @@
+import { Header, } from 'semantic-ui-react';
+
+const Home = () => (
+  <Header as="h3" textAlign="center">Devise Auth App</Header>
+)
+
+export default Home;
--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
+import AuthProvider from './providers/AuthProvider';
+import { initMiddleware } from 'devise-axios';
+
+initMiddleware();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
